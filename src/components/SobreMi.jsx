ee1f0a5f562b1b55f18baee29aae9affaa2ddf75
Esteban Sayago <sayagoo823@gmail.com>
2023-01-25 10:11:14 -0300

    feat: crear componente 'sobre mi'

---
+++ src/components/SobreMi.jsx
@@ -0,0 +1,12 @@
+function SobreMi(){
+    return (
+        <>
+            <div className="inline-block space-y-4 max-w-sm p-2">
+                <h2 className="text-center">¡ Hey por aqui 😁 !</h2>
+                <p>¡ Saludos 👋 ! Soy Desarrollador Trainer/Junior, con 1 año de experiencia en el desarrollo web. Te invito a continuar revisando mi portafolio y a que conozcas mas sobre mi.</p>
+            </div>
+        </>
+    )
+}
+
+export default SobreMi
