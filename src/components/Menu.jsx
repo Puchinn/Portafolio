98db88ab4ce2d31e5a981a9b514ebf8b954516e7
Esteban Sayago <sayagoo823@gmail.com>
2023-01-26 09:26:52 -0300

    feat: crear componente menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,22 @@
+function Menu(){
+    return(
+        <>
+            <nav className="inline-flex gap-x-3 flex-col [&_label_div]:transition-all [&_label_div]:cursor-pointer [&_label]:inline-block ">
+                <label htmlFor="home">
+                    <input type="radio" name="menu" id="home" className="peer" hidden defaultChecked/>
+                    <div className="inline-block py-20 px-7 bg-rose-500 peer-checked:translate-x-4 rounded-lg"></div>
+                </label>
+                <label htmlFor="proyectos">
+                    <input type="radio" name="menu" id="proyectos" className="peer" hidden/>
+                    <div className="inline-block py-20 px-7 bg-rose-500 peer-checked:translate-x-4 rounded-lg"></div>
+                </label>
+                <label htmlFor="sobremi">
+                    <input type="radio" name="menu" id="sobremi" className="peer" hidden />
+                    <div className="inline-block py-20 px-7 bg-rose-500 peer-checked:translate-x-4 rounded-lg"></div>
+                </label>
+            </nav>
+        </>
+    )
+}
+
+export default Menu
