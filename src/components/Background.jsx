7ea02f43a0e4b7b419d663b71d0a21dd54e3a44f
Esteban Sayago <sayagoo823@gmail.com>
2023-01-27 08:53:43 -0300

    feat: crear componente background del portafolio

---
+++ src/components/Background.jsx
@@ -0,0 +1,10 @@
+function Background(){
+    return(
+        <>
+            <div className="absolute top-0 left-0 w-screen h-screen bg-gradient-to-r from-[#ad6fc2] to-[#64e5ff] -z-10">
+            </div>
+        </>
+    )
+}
+
+export default Background
