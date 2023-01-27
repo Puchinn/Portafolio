d925fa3c94c70e9905569daeb53d03e97276199f
Esteban Sayago <sayagoo823@gmail.com>
2023-01-27 12:52:16 -0300

    style: mejorar apariencia del componente

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -4,8 +4,8 @@ function Contenedor({children}){
     return(
         <>
             <div className="w-screen h-screen flex justify-center items-center">
-                <div className="flex -space-x-6 w-full justify-center ">
-                    <div className="p-9 w-5/6 rounded-3xl z-10 bg-opacity-50 bg-slate-500">
+                <div className="flex w-full justify-center ">
+                    <div className="p-9 w-5/6 rounded-3xl bg-opacity-50 bg-white">
                         {children}
                     </div>
                     <div className="self-center">
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,15 +4,15 @@ function Menu(){
             <nav className="inline-flex gap-x-3 flex-col [&_label_div]:transition-all [&_label_div]:cursor-pointer [&_label]:inline-block ">
                 <label htmlFor="home">
                     <input type="radio" name="menu" id="home" className="peer" hidden defaultChecked/>
-                    <div className="inline-block py-20 px-7 bg-rose-500 peer-checked:translate-x-4 rounded-lg"></div>
+                    <div className="inline-block py-16 px-5 bg-white opacity-30 peer-checked:px-7 peer-checked:opacity-50 rounded-r-lg"></div>
                 </label>
                 <label htmlFor="proyectos">
                     <input type="radio" name="menu" id="proyectos" className="peer" hidden/>
-                    <div className="inline-block py-20 px-7 bg-rose-500 peer-checked:translate-x-4 rounded-lg"></div>
+                    <div className="inline-block py-16 px-5 bg-white opacity-30 peer-checked:px-7 peer-checked:opacity-50 rounded-r-lg"></div>
                 </label>
                 <label htmlFor="sobremi">
                     <input type="radio" name="menu" id="sobremi" className="peer" hidden />
-                    <div className="inline-block py-20 px-7 bg-rose-500 peer-checked:translate-x-4 rounded-lg"></div>
+                    <div className="inline-block py-16 px-5 bg-white opacity-30 peer-checked:px-7 peer-checked:opacity-50 rounded-r-lg"></div>
                 </label>
             </nav>
         </>
