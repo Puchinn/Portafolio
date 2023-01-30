b03398f8ba5ea56ec61be448a363ace7ed5b979a
Esteban Sayago <sayagoo823@gmail.com>
2023-01-30 09:35:23 -0300

    feat:crear componente botonmenu

---
+++ src/components/BotonMenu.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+function BotonMenu({ name, isActive, onClick }){
+    return(
+        <>  
+            <Link
+                onClick={()=> onClick(name)}
+                to={name}
+                className={`w-11 bg-white inline-block rounded-r-xl h-32 transition-all opacity-30
+                ${isActive ? ' opacity-50 w-16': ''}`}
+                >   
+            </Link>
+        </>
+    )
+}
+
+export default BotonMenu
