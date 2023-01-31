52d794230eb270d3fa2a446fa522a302c88276a7
Esteban Sayago <sayagoo823@gmail.com>
2023-01-31 11:24:13 -0300

    feat: agregar estado al componente menu para controlar botones activos

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,33 @@
+import BotonMenu from "./BotonMenu"
+import { useState } from "react"
+
 function Menu(){
+
+    const [activeButton, setActiveButton] = useState('/');
+    const handleButtonClick = (name) => {
+        setActiveButton(name);
+    }
+
+
+
     return(
         <>
-            <nav className="inline-flex gap-x-3 flex-col [&_label_div]:transition-all [&_label_div]:cursor-pointer [&_label]:inline-block ">
-                <label htmlFor="home">
-                    <input type="radio" name="menu" id="home" className="peer" hidden defaultChecked/>
-                    <div className="inline-block py-16 px-5 bg-white opacity-30 peer-checked:px-7 peer-checked:opacity-50 rounded-r-lg"></div>
-                </label>
-                <label htmlFor="proyectos">
-                    <input type="radio" name="menu" id="proyectos" className="peer" hidden/>
-                    <div className="inline-block py-16 px-5 bg-white opacity-30 peer-checked:px-7 peer-checked:opacity-50 rounded-r-lg"></div>
-                </label>
-                <label htmlFor="sobremi">
-                    <input type="radio" name="menu" id="sobremi" className="peer" hidden />
-                    <div className="inline-block py-16 px-5 bg-white opacity-30 peer-checked:px-7 peer-checked:opacity-50 rounded-r-lg"></div>
-                </label>
+            <nav className="flex flex-col space-y-5">
+                <BotonMenu 
+                    name="/" 
+                    isActive={activeButton === '/'} 
+                    onClick={handleButtonClick}
+                />
+                <BotonMenu 
+                    name="proyectos" 
+                    isActive={activeButton === 'proyectos'} 
+                    onClick={handleButtonClick}
+                />
+                <BotonMenu 
+                    name="contacto" 
+                    isActive={activeButton === 'contacto'} 
+                    onClick={handleButtonClick}
+                />
             </nav>
         </>
     )
