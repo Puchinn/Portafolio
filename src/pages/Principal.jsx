0aa0452b4ae3626462be530923cd9333f9134120
Esteban Sayago <sayagoo823@gmail.com>
2023-01-31 11:33:46 -0300

    refactor: remplazar nombre de componente 'Tarjeta' por 'Perfil'

--- src/pages/Principal.jsx
+++ src/pages/Principal.jsx
@@ -1,7 +1,7 @@
 import BotonDM from "../components/BotonDM"
 import SobreMi from "../components/SobreMi"
 import Tecnologias from "../components/Tecnologias"
-import Tarjeta from "../components/Tarjeta"
+import Perfil from "../components/Perfil"
 import Titulo from "../components/Titulo"
 
 function Principal(){
@@ -13,7 +13,7 @@ function Principal(){
                     </div>
                     <div className="flex justify-around items-center">
                         <div className="flex flex-col">
-                            <Tarjeta />
+                            <Perfil />
                             <SobreMi />
                         </div>
                         <div className="flex flex-col justify-between space-y-16  items-center">
