ab589ab3b00159cad322f75a540d43178a0f29c4
Esteban Sayago <sayagoo823@gmail.com>
2023-01-31 11:28:03 -0300

    style: agregar una altura determinada al contenedor

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -5,7 +5,7 @@ function Contenedor(){
     return(
         <>
             <div className="w-screen h-screen flex justify-center items-center">
-                <div className="flex w-full justify-center ">
+                <div className="flex w-full justify-center h-5/6">
                     <div className="p-9 w-5/6 rounded-3xl bg-opacity-50 bg-white">
                         <Outlet />
                     </div>
