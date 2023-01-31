a6d873a881f4caadfa5daf7613db2e98a871e332
Esteban Sayago <sayagoo823@gmail.com>
2023-01-31 11:17:52 -0300

    feat: crear pagina para seccion proyectos

---
+++ src/pages/Proyectos.jsx
@@ -0,0 +1,13 @@
+function Proyectos(){
+    return(
+        <>
+            <div>
+                <h1 className="text-6xl">Proyectos</h1>
+                <div className="flex justify-around">
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Proyectos
