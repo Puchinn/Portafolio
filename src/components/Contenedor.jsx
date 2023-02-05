92785011a6af2d6c547a6f27cc0b1aafb82eef38
Esteban Sayago <sayagoo823@gmail.com>
2023-02-05 09:46:10 -0300

    refactor: reducir lineas de codigo innecesarias

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -5,16 +5,12 @@ function Contenedor(){
     return(
         <>
             <div className="w-screen h-screen flex justify-center items-center">
-                <div className="flex w-full justify-center h-5/6">
-                    <div className="p-9 w-5/6 rounded-sm bg-opacity-50 bg-white relative">
-                    <div class="h-[95%] w-6 bg-white bottom-0 absolute -left-6 bg-opacity-20 rounded-tl-sm"></div>
-                    <div class="h-4 w-[95%] bg-white -bottom-4 absolute -left-6 bg-opacity-20 rounded-br-sm rounded-bl-sm"></div>
-                        <Outlet />
-                    </div>
-                    <div className="self-center">
-                        <Menu />
-                    </div>
+                <div className="p-9 w-5/6 min-h-[800px] rounded-sm bg-opacity-50 bg-white relative">
+                    <div className="h-[95%] w-6 bg-white bottom-0 absolute -left-6 bg-opacity-20 rounded-tl-sm"></div>
+                    <div className="h-4 w-[95%] bg-white -bottom-4 absolute -left-6 bg-opacity-20 rounded-br-sm rounded-bl-sm"></div>
+                    <Outlet />
                 </div>
+                <Menu />
             </div>
         </>
     )
