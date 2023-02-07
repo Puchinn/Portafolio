d56a61d741f3b26d370aeed7b2bd8cb9431950f4
Esteban Sayago <sayagoo823@gmail.com>
2023-02-07 09:47:11 -0300

    feat: agregar nuevas paginas Inicio y Sobre mi

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,19 @@
+function Inicio() {
+    return (
+        <>
+            <div className="text-center">
+                <p className="text-2xl text-gray-700">Esteban Sayago</p>
+                <h1 className="text-7xl">Desarrollador Web</h1>
+                <h1 className="text-6xl">Front-End</h1>
+                <div className="space-x-5 pt-5">
+                    <button className="rounded-sm border border-white bg-slate-600 bg-opacity-30 py-2 px-4 text-white">
+                        Ver Portafolio
+                    </button>
+                    {/* <button className="py-2 px-4 bg-opacity-30 bg-slate-600 border rounded-sm border-white text-white" >GitHub</button> */}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Inicio
---
+++ src/pages/SobreMi.jsx
@@ -0,0 +1,24 @@
+import SobreMitext from "../components/SobreMi"
+import Foto from "../components/Foto"
+import Teconologias from "../components/Tecnologias"
+
+function SobreMi() {
+    return (
+        <>
+            <div className="max-w-5xl">
+                <h1 className="py-5 pl-10 text-4xl font-thin text-slate-700">
+                    Sobre Mi:
+                </h1>
+                <div className="flex h-2/3 items-center justify-between gap-x-5 border-y border-gray-300 p-5 py-12">
+                    <Foto />
+                    <div className="flex flex-col justify-between border-x-2 border-gray-700 p-4">
+                        <SobreMitext />
+                        <Teconologias />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SobreMi
