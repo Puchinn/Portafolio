944f609191702e9de390d3c5c9427da99463315d
Esteban Sayago <sayagoo823@gmail.com>
2023-02-06 21:05:53 -0300

    feat: agregar estilos 'dark mode' al background del portafolio

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,8 +1,7 @@
-function Background(){
-    return(
+function Background() {
+    return (
         <>
-            <div className="absolute top-0 left-0 w-screen h-screen bg-gradient-to-r from-[#ad6fc2] to-[#64e5ff] -z-10">
-            </div>
+            <div className="absolute top-0 left-0 -z-10 h-screen w-screen bg-gradient-to-r from-[#ad6fc2] to-[#64e5ff] dark:bg-gradient-to-t dark:from-gray-900 dark:to-emerald-900"></div>
         </>
     )
 }
