9eeef5429485dfd71fe60fee5e1d284e8b614340
Esteban Sayago <sayagoo823@gmail.com>
2023-02-07 10:56:41 -0300

    refactor: utilizar componente router customizado

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import './main.css'
-import router from './router/router'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Router from "./router/router"
+import "./main.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Router />
+    </React.StrictMode>
 )
