c156890f41d3c6d211e1b3470d7b144513af253e
Esteban Sayago <sayagoo823@gmail.com>
2023-02-08 10:57:24 -0300

    style: ajustar estilos y agregar componentes tecnologias

--- src/components/Tecnologias.jsx
+++ src/components/Tecnologias.jsx
@@ -1,14 +1,16 @@
-
+import TechsFront from "./TechsFront"
+import TechsOtras from "./TechsOtras"
 
 function Teconologias() {
     return (
         <>
-            <div>
-                <h1 className="text-xl tracking-wider text-gray-900">
-                    Tecnologias que utilizo:
+            <div className="space-y-2">
+                <h1 className="text-lg tracking-wider text-gray-900">
+                    Tecnologias con las que trabajo:
                 </h1>
                 <div className="flex justify-around">
-                    
+                    <TechsFront />
+                    <TechsOtras />
                 </div>
             </div>
         </>
