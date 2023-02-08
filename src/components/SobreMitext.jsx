1d4632c072db00282af4270320eb53e3ef29390c
Esteban Sayago <sayagoo823@gmail.com>
2023-02-08 10:58:00 -0300

    feat: agregar estilos y cambiar el contenido del texto

--- src/components/SobreMitext.jsx
+++ src/components/SobreMitext.jsx
@@ -1,10 +1,15 @@
 function SobreMitext() {
     return (
         <>
-            <p className="text-left indent-2 text-xl tracking-wider text-gray-900">
-                ¡ Saludos 👋 ! Soy Desarrollador Trainer/Junior , con 1 año de
-                experiencia en el desarrollo web. Te invito a continuar
-                revisando mi portafolio y a que conozcas mas sobre mi.
+            <p className="border-b border-dotted py-3 text-left indent-2 text-lg tracking-wider text-gray-900">
+                ¡ Saludos 👋 ! <br></br> Me llamo Esteban Sayago Fernando, soy
+                desarrollador web
+                <span className="mx-1 -skew-y-6 bg-rose-500 p-1 text-white">
+                    trainer/junior
+                </span>
+                con 1 año de experiencia. Actualmente cumpliendo un stack de
+                front-end, pero me gustaria y mi objetivo es ser un
+                desarrollador web full stack.
             </p>
         </>
     )
