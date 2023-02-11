59737fc8c988e403c75690c6e804ac3eec221fe5
Esteban Sayago <sayagoo823@gmail.com>
2023-02-11 11:31:40 -0300

    refactor: cambiar el display grid por flex

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -5,10 +5,10 @@ function Contenedor() {
     return (
         <>
             <div className="flex h-screen w-screen items-center justify-center">
-                <div className="relative grid min-h-[800px] w-5/6 items-center justify-center rounded-sm bg-white bg-opacity-50 p-9">
+                <div className="relative flex min-h-[800px] w-5/6 items-center justify-center rounded-sm bg-white bg-opacity-50">
+                    <Outlet />
                     <div className="absolute bottom-0 -left-6 h-[95%] w-6 rounded-tl-sm bg-white bg-opacity-20"></div>
                     <div className="absolute -bottom-4 -left-6 h-4 w-[95%] rounded-br-sm rounded-bl-sm bg-white bg-opacity-20"></div>
-                    <Outlet />
                 </div>
                 <Menu />
             </div>
