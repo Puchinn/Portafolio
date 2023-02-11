96562e64d4f8d61254f03bc12fb8f7982a629e5a
Esteban Sayago <sayagoo823@gmail.com>
2023-02-11 12:06:27 -0300

    refactor:utilizar los componentes botones y agregar estado

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -1,42 +1,32 @@
-import { useState } from "react";
-import Tarjeta from "../components/Tarjeta";
-import Proyecto from "../components/Proyecto";
+import BtnBack from "../components/BtnBack"
+import BtnNext from "../components/BtnNext"
+import Proyecto from "../components/Proyecto"
+import proyectos from "../info/listaProyectos"
+import { useState } from "react"
 
-function Proyectos(){
-
-    const [selected,setSelected] = useState("Genshin Impact");
-
-    const proyectos = [
-        {
-            src:"https://i0.wp.com/www.gamerfocus.co/wp-content/uploads/2022/09/genshin-impact-parche-actualizacion-version-3-1-personajes-sumeru-eventos-armas-anime-gamerfocus.jpg?fit=1920%2C1080&ssl=1",
-            titulo:"Genshin Impact",
-            desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil repellendus fugit, hic asperiores eos minus debitis nisi blanditiis quos deserunt."
-        },
-        {
-            src:"https://i.ytimg.com/vi/H2LaRCieieM/maxresdefault.jpg",
-            titulo:"Hu Tao",
-            desc:"Loremcing e asdas dasdasdasd asd asd asda sdasdasd asdasdasd fugit, hic asperiores eos minus debitis nisi blanditiis quos deserunt."
-        }
-    ]
-
-    const activo = proyectos.find(elemento => elemento.titulo === selected)
+function Proyectos() {
+    const long = proyectos.length - 1
+    const [idActual, setId] = useState(0)
 
     return (
         <>
-            <div className="w-full h-full relative">
-                <h1 className="text-6xl absolute top-0">Proyectos</h1>
-                <div className="flex w-full h-full ">
-                    <div className='w-5/12 flex flex-col justify-center space-y-5 pl-12'>
-
-                        {
-                            proyectos.map(elemento =>{
-                                return <Tarjeta {...elemento} eActivo={selected} setActivo={setSelected} key={elemento.titulo} />
-                            })
-                        }
-
-                    </div>
-                    <div className='w-full flex justify-center items-center'>
-                        <Proyecto {...activo} />
+            <div className="w-5/6">
+                <h1 className="py-5 pl-10 text-4xl font-thin text-slate-700">
+                    Proyectos:
+                </h1>
+                <div className="w-full border-y border-gray-300 p-5 py-6">
+                    <div className="flex w-full items-center">
+                        <BtnBack
+                            onClick={setId}
+                            idActual={idActual}
+                            long={long}
+                        />
+                        <Proyecto {...proyectos[idActual]} />
+                        <BtnNext
+                            onClick={setId}
+                            idActual={idActual}
+                            long={long}
+                        />
                     </div>
                 </div>
             </div>
