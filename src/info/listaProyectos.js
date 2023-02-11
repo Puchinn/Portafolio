7bc8101b45a95a19e88411ac368db7d778501160
Esteban Sayago <sayagoo823@gmail.com>
2023-02-11 11:58:01 -0300

    feat: archivo para contener informacion acerca de los proyectos

---
+++ src/info/listaProyectos.js
@@ -0,0 +1,22 @@
+const proyectos = [
+    {
+        src: "https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+        titulo: "Genshin Impact",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil repellendus fugit, hic asperiores eos minus debitis nisi blanditiis quos deserunt.",
+        links: {
+            github: "",
+            demo: "",
+        },
+    },
+    {
+        src: "https://i.ytimg.com/vi/H2LaRCieieM/maxresdefault.jpg",
+        titulo: "Hu Tao",
+        desc: "Loremcing e asdas dasdasdasd asd asd asda sdasdasd asdasdasd fugit, hic asperiores eos minus debitis nisi blanditiis quos deserunt.",
+        links: {
+            github: "",
+            demo: "",
+        },
+    },
+]
+
+export default proyectos
