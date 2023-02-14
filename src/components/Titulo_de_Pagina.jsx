f452073c96126d4f98d9f349f0a6b980f2763ce4
Esteban Sayago <sayagoo823@gmail.com>
2023-02-13 22:33:38 -0300

    feat: crear componente titulo para todas las paginas

---
+++ src/components/Titulo_de_Pagina.jsx
@@ -0,0 +1,9 @@
+function Titulo_de_Pagina({titulo}) {
+    return (
+        <h1 className="py-5 pl-10 text-4xl font-thin text-slate-700">
+            {titulo}
+        </h1>
+    )
+}
+
+export default Titulo_de_Pagina
