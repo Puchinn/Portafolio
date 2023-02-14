24e5d50a29f2ea55e08b9c26bc532dd427931f43
Esteban Sayago <sayagoo823@gmail.com>
2023-02-14 15:48:16 -0300

    feat: crear pagina de intro al portafolio

---
+++ src/pages/Intro.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom"
+
+function Intro() {
+    return (
+        <div className="fondo_animado grid h-screen w-full animate-backanimado items-center bg-black text-center">
+            <div className="animate-text-focus-in">
+                <p className="font-serif text-xl tracking-wider text-slate-500 ">
+                    Esteban Sayago
+                </p>
+                <h1 className="text-7xl text-white">Desarrollador Web</h1>
+                <h1 className="text-6xl text-rose-600">Front-End</h1>
+                <Link
+                    to="portafolio/portafolio"
+                    class="group relative mt-5 inline-block animate-wobble-hor-bottom text-sm font-medium text-white focus:outline-none focus:ring"
+                >
+                    <span class="absolute inset-0 border border-rose-600 group-active:border-rose-500"></span>
+                    <span class="block animate-tracking-in-contract border border-rose-600 bg-rose-600 px-12 py-3 transition-transform active:border-rose-500 active:bg-rose-500 group-hover:-translate-x-1 group-hover:-translate-y-1">
+                        Ver Portafolio
+                    </span>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Intro
