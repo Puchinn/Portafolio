4d299e8f9b8bf080f332512eaeae1334373b796b
Esteban Sayago <sayagoo823@gmail.com>
2023-02-13 23:35:31 -0300

    fix: arreglar error de importacion

--- src/pages/Seccion_Proyectos/Proyectos.jsx
+++ src/pages/Seccion_Proyectos/Proyectos.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import Mostrar_Proyecto_Actual from "./components/Mostrar_Proyecto_Actual"
 import lista_de_proyectos from "../../info/listaProyectos"
 import Titulo_de_Pagina from "../../components/Titulo_de_Pagina"
-import Boton_Mover_Proyectos from "./components/Boton_mover_proyectos"
+import Boton_Mover_Proyectos from "./components/Boton_Mover_Proyectos"
 
 function Proyectos() {
     const longitud_lista = lista_de_proyectos.length - 1
