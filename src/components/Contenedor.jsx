6e2de7ff198771e88dcb4651177d112eb26f73e3
Esteban Sayago <sayagoo823@gmail.com>
2023-02-15 17:07:24 -0300

    mostrar contenido

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -6,7 +6,10 @@ function Contenedor() {
     return (
         <>
             <div className="flex h-screen w-screen items-center justify-center">
-                <div className="relative flex min-h-[700px] w-3/4 items-center justify-center rounded-2xl bg-white bg-opacity-50 dark:border-none dark:bg-black dark:bg-opacity-50 "></div>
+                <div className="relative flex min-h-[700px] w-3/4 items-center justify-center rounded-2xl bg-white bg-opacity-50 dark:border-none dark:bg-black dark:bg-opacity-50 ">
+                    <Outlet />
+                </div>
+                <Menu />
                 <BotonDM />
             </div>
         </>
