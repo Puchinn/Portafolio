b88c772977a466da29b3bf3732e36945d6cbc2bd
Esteban Sayago <sayagoo823@gmail.com>
2023-02-15 11:59:11 -0300

    feat: modo claro y modo oscuro

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,6 +3,7 @@ function Background() {
     return (
         <>
             <div className="absolute top-0 left-0 -z-10 h-screen w-screen bg-no-repeat bg-cover"  style={{backgroundImage:`url(${bg})`}}></div>
+            <div className="absolute h-screen w-screen -z-10 bg-black top-0 bg-opacity-40 hidden dark:block"></div>
         </>
     )
 }
--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -1,12 +1,13 @@
 import Menu from "./Menu"
 import { Outlet } from "react-router-dom"
+import BotonDM from "./BotonDM"
 
 function Contenedor() {
     return (
         <>
             <div className="flex h-screen w-screen items-center justify-center">
-                <div className="relative flex min-h-[700px] w-3/4 items-center justify-center bg-white bg-opacity-60 rounded-2xl border backdrop-blur-sm">
-                </div>
+                <div className="relative flex min-h-[700px] w-3/4 items-center justify-center rounded-2xl bg-white bg-opacity-50 dark:border-none dark:bg-black dark:bg-opacity-50 "></div>
+                <BotonDM />
             </div>
         </>
     )
