20e2292615249aef4417c2f7591ae030db856ace
Esteban Sayago <sayagoo823@gmail.com>
2023-02-16 12:53:38 -0300

    style: ajustar ancho maximo y eliminar boton darkmode

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -6,11 +6,10 @@ function Contenedor() {
     return (
         <>
             <div className="flex h-screen w-screen items-center justify-center">
-                <div className="relative flex min-h-[700px] w-3/4 items-center justify-center rounded-2xl bg-white bg-opacity-50 dark:border-none dark:bg-black dark:bg-opacity-50 ">
+                <div className="relative flex max-w-7xl min-h-[700px] w-3/4 items-center justify-center overflow-hidden rounded-2xl bg-white bg-opacity-50 dark:border-none dark:bg-black dark:bg-opacity-50 ">
                     <Outlet />
                 </div>
                 <Menu />
-                <BotonDM />
             </div>
         </>
     )
