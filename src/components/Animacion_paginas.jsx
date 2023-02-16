ad988ea90dafb9d7fa03aaa935d41aafdfb50091
Esteban Sayago <sayagoo823@gmail.com>
2023-02-16 12:52:40 -0300

    feat: crear componente para animar las paginas

---
+++ src/components/Animacion_paginas.jsx
@@ -0,0 +1,16 @@
+import { motion } from "framer-motion"
+
+function Animacion_paginas({ children }) {
+    return (
+        <motion.div
+        className="w-full"
+            initial={{ translateY: "150%" }}
+            animate={{ translateY: 0 }}
+            transition={{ duration: 0.6, ease: "backInOut" }}
+        >
+            {children}
+        </motion.div>
+    )
+}
+
+export default Animacion_paginas
