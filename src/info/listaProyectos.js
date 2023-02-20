d09069b8dc1c4858abdc0e794ac067329a8aedbe
Esteban Sayago <sayagoo823@gmail.com>
2023-02-20 13:29:26 -0300

    refactor: eliminar informacion de ejemplo

--- src/info/listaProyectos.js
+++ src/info/listaProyectos.js
@@ -1,7 +1,7 @@
 const lista_de_proyectos = [
     {
-        src: "https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-        titulo: "Genshin Impact",
+        src: "",
+        titulo: "Ejemplo",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil repellendus fugit, hic asperiores eos minus debitis nisi blanditiis quos deserunt.",
         links: {
             github: "",
@@ -9,8 +9,8 @@ const lista_de_proyectos = [
         },
     },
     {
-        src: "https://i.ytimg.com/vi/H2LaRCieieM/maxresdefault.jpg",
-        titulo: "Hu Tao",
+        src: "",
+        titulo: "Ejemplo 1",
         desc: "Loremcing e asdas dasdasdasd asd asd asda sdasdasd asdasdasd fugit, hic asperiores eos minus debitis nisi blanditiis quos deserunt.",
         links: {
             github: "",
