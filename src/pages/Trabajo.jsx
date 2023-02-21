0baf1b160d7771fb25b16fda008f68848e396f2c
Esteban Sayago <sayagoo823@gmail.com>
2023-02-20 21:23:30 -0300

    nuevo archivo temporal

---
+++ src/pages/Trabajo.jsx
@@ -0,0 +1,11 @@
+import Titulo_de_Pagina from "../components/Titulo_de_Pagina"
+
+function Trabajo(){
+    return (
+        <div className="">
+            <Titulo_de_Pagina  titulo={"Portafolio"} />
+        </div>
+    )
+}
+
+export default Trabajo
