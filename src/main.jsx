3f3b653f6e0dfff2b1793ddb91f864de95726622
Esteban Sayago <sayagoo823@gmail.com>
2023-02-21 12:16:40 -0300

    refactor: implementar el contenedor del context en el main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import Router from "./router/router"
 import "./main.css"
+import ContextContainer from "./context/darkTheme"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Router />
+        <ContextContainer>
+            <Router />
+        </ContextContainer>
     </React.StrictMode>
 )
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,17 +4,12 @@ import App from "../App"
 import Contenedor from "../components/Contenedor"
 import Sobre_Mi from "../pages/Sobre_Mi/Sobre_Mi"
 import Contacto from "../pages/Contacto/Contacto"
-import ContextContainer from "../context/darkTheme"
 import Trabajo from "../pages/Trabajo"
 
 const rutas = createBrowserRouter([
     {
         path: "/",
-        element: (
-            <ContextContainer>
-                <App />
-            </ContextContainer>
-        ),
+        element: <App />,
         children: [
             {
                 path: "/",
