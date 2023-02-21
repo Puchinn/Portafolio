edd76041250ac9c85a3dc1ec83ba8e32c9797742
Esteban Sayago <sayagoo823@gmail.com>
2023-02-21 12:15:29 -0300

    agregar estado para controlar la salida de notificacion

--- src/pages/Contacto/Formulario.jsx
+++ src/pages/Contacto/Formulario.jsx
@@ -1,47 +1,40 @@
-function Formulario() {
-    return (
-        <div className="w-full rounded-2xl border-4 border-[#b0c2f2] dark:border-white py-3 px-4 ">
-            <h1 className="text-center text-xl text-gray-700 tracking-wide dark:text-white ">Formulario de contacto</h1>
+import { useForm } from "@formspree/react"
+import { useState } from "react"
+import Input_nombre from "./components/Input_nombre"
+import Input_email from "./components/Input_email"
+import Input_textarea from "./components/Input_textarea"
+import Notificacion from "./components/Notificacion"
 
-            <form action="" className="mx-auto mt-2 mb-0 space-y-2">
-                <div className="flex flex-col text-start">
-                    <label className="text-base text-gray-700 dark:text-white" for="nombre">
-                        Nombre :
-                    </label>
-                    <input
-                        id="nombre"
-                        type="text"
-                        className=" rounded-md border-4 border-gray-300 dark:text-black p-3 text-base outline-none"
-                        placeholder="Escribe tu nombre"
-                    />
-                </div>
-                <div className="flex flex-col text-start">
-                    <label className="text-base text-gray-700 dark:text-white" for="email">
-                        Email :
-                    </label>
-                    <input
-                        id="email"
-                        type="email"
-                        className=" rounded-md border-4 border-gray-300 dark:text-black p-3 text-base outline-none"
-                        placeholder="Escribe tu email"
-                    />
-                </div>
-                <div className="flex flex-col text-start">
-                    <label className="text-base text-gray-700 dark:text-white" for="mensanje">
-                        Mensaje :
-                    </label>
-                    <textarea
-                        placeholder="Ej: Me gustaria trabajar contigo"
-                        className="rounded-md border-4 border-gray-300 dark:text-black p-3 text-base outline-none"
-                        id="mensaje"
-                        cols="30"
-                        rows="5"
-                    ></textarea>
-                </div>
-                <button className="w-full text-lg rounded-lg bg-blue-500 hover:bg-blue-400 py-2 text-white">
+function Formulario() {
+    const [state, handleSubmit] = useForm("xjvdwojz")
+    const [mostrar, setMostrar] = useState(true)
+
+    return (
+        <div className="w-full rounded-2xl border-4 border-[#b0c2f2] py-3 px-4 dark:border-white ">
+            <h1 className="text-center text-xl tracking-wide text-gray-700 dark:text-white ">
+                Formulario de contacto
+            </h1>
+
+            <form
+                onSubmit={handleSubmit}
+                className="mx-auto mt-2 mb-0 space-y-2"
+            >
+                <Input_nombre />
+                <Input_email />
+                <Input_textarea />
+
+                <button
+                    disabled={state.submitting}
+                    type="submit"
+                    className="w-full rounded-lg bg-blue-500 py-2 text-lg text-white hover:bg-blue-400"
+                >
                     Enviar
                 </button>
             </form>
+
+            {state.succeeded && mostrar && (
+                <Notificacion setMostrar={setMostrar} />
+            )}
         </div>
     )
 }
