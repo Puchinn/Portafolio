c400f9d2c8b113fbdd7f2b98dc3a3399ff9e920e
Esteban Sayago <sayagoo823@gmail.com>
2023-02-20 21:22:31 -0300

    style: reducir el blanco del texto para remarcar los estilos strong del texto

--- src/pages/Sobre_Mi/components/Texto_Sobre_Mi.jsx
+++ src/pages/Sobre_Mi/components/Texto_Sobre_Mi.jsx
@@ -3,23 +3,27 @@ import Iconos_Herramientas from "./Iconos_Herramientas"
 
 function Texto_Sobre_Mi() {
     return (
-        <p className="px-4 text-left indent-2 text-lg tracking-wider text-gray-900 dark:text-white">
+        <p className="px-4 text-left indent-2 text-lg tracking-wider text-gray-900 dark:text-gray-100">
             ¡Encantado de conocerte! 👋. <br></br>
             Soy Esteban, un estudiante autodidacta del mundo del desarrollo web.
             Me dedico a estudiar la creación de sitios y aplicaciones web, con
             objetivos claros de ser un desarrollador web full-stack.
             <br /> <br />
             Tengo habilidades en tecnologías del desarrollo front-end, como
-            <strong> HTML, CSS, JavaScript, React y TailwindCSS</strong>, dando
-            por obvio <strong> Git y GitHub</strong>.
+            <strong className="dark:text-white">
+                {" "}
+                HTML, CSS, JavaScript, React y TailwindCSS
+            </strong>
+            , dando por obvio{" "}
+            <strong className="dark:text-white"> Git y GitHub</strong>.
             <br /> <br />
             Me interesa aprender tecnologías famosas y que tienen años en el
-            mercado, como pueden ser NextJs, Python o también ExpressJs para aprender
-            back-end y MongoDB para el lenguaje de consulta de base de datos.
-            También quiero aprender metodologías ágiles de desarrollo de
+            mercado, como pueden ser NextJs, Python o también ExpressJs para
+            aprender back-end y MongoDB para el lenguaje de consulta de base de
+            datos. También quiero aprender metodologías ágiles de desarrollo de
             software y buenas prácticas para escribir código, como pueden ser{" "}
-            <strong> Scrum</strong> y tomar lo mejor del{" "}
-            <strong> Clean Code</strong>.
+            <strong className="dark:text-white"> Scrum</strong> y tomar lo mejor
+            del <strong className="dark:text-white"> Clean Code</strong>.
             <br />
             <br />
             También tengo algunos proyectos en mente, como por ejemplo un
