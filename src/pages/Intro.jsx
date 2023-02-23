32f74f7fccc17f7c323cd2cf9578799698b7008f
Esteban Sayago <sayagoo823@gmail.com>
2023-02-23 11:16:58 -0300

    refactor: eliminar animaciones innecesarias

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -4,21 +4,21 @@ import { ThemeContext } from "../context/darkTheme"
 
 
 function Intro() {
-    const [color] = useContext(ThemeContext)
+    const [color,] = useContext(ThemeContext)
     return (
         <div className={`${color === "dark" ? "fondo_animado_oscuro" : "fondo_animado_claro"}  grid h-screen w-full animate-backanimado items-center text-center`}>
             <div className="animate-text-focus-in">
                 <p className="font-serif text-xl tracking-wider text-slate-500 ">
                     Esteban Sayago
                 </p>
-                <h1 className="text-7xl text-white">Desarrollador Web</h1>
-                <h1 className="text-6xl text-rose-600">Front-End</h1>
+                <h1 className="text-5xl sm:text-7xl text-white">Desarrollador Web</h1>
+                <h1 className="text-4xl sm:text-6xl text-rose-600">Front-End</h1>
                 <Link
                     to="portafolio/sobremi"
-                    className="group relative mt-5 inline-block animate-wobble-hor-bottom text-sm font-medium text-white focus:outline-none focus:ring"
+                    className="group relative mt-5 inline-block text-sm font-medium text-white focus:outline-none focus:ring"
                 >
                     <span className="absolute inset-0 border border-rose-600 group-active:border-rose-500"></span>
-                    <span className="block animate-tracking-in-contract border border-rose-600 bg-rose-600 px-12 py-3 transition-transform active:border-rose-500 active:bg-rose-500 group-hover:-translate-x-1 group-hover:-translate-y-1">
+                    <span className="block border border-rose-600 bg-rose-600 px-12 py-3 transition-transform active:border-rose-500 active:bg-rose-500 group-hover:-translate-x-1 group-hover:-translate-y-1">
                     Conocer Perfil
                     </span>
                 </Link>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,8 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        "text-focus-in": "text-focus-in 1.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) 0.5s both",
-        "tracking-in-contract": "tracking-in-contract 2.5s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.5s both",
-        "backanimado": "backanimado 30s ease infinite",
-        "wobble-hor-bottom": "wobble-hor-bottom 0.8s 1 2.8s both"
+        "text-focus-in": "text-focus-in 1.7s cubic-bezier(0.550, 0.085, 0.680, 0.530) 0.5s both",
+        "backanimado": "backanimado 30s ease infinite"
       }
       ,
       keyframes: {
@@ -15,48 +13,11 @@ module.exports = {
           "0%": { filter: "blur(9px)", opacity: "0" },
           "100%": { filter: "blur(0px)", opacity: "1" }
         },
-        "tracking-in-contract": {
-          "0%": {
-            "letter-spacing": "1em",
-            opacity: "0"
-          },
-          "40%": {
-            opacity: "0.6"
-          }
-          ,
-          "100%": {
-            "letter-spacing": "normal",
-            opacity: "1"
-          }
-        },
         "backanimado": {
           "0%": { "background-position": "99% 0%" },
           "50%": { "background-position": "2% 100%" },
           "100%": { "background-position": "99% 0%" }
         },
-        "wobble-hor-bottom": {
-          "0%": {},
-          "100%": {
-            transform: "translateX(0%)",
-            "transform-origin": "50% 50%"
-          },
-          "15%": {
-            transform: "translateX(-30px) rotate(-6deg)"
-          },
-          "30%": {
-            transform: "translateX(15px) rotate(6deg)"
-          },
-          "45%": {
-            transform: "translateX(-15px) rotate(-3.6deg)"
-          },
-          "60%": {
-            transform: "translateX(9px) rotate(2.4deg)"
-          },
-          "75%": {
-            transform: "translateX(-6px) rotate(-1.2deg)"
-          }
-        }
-
       }
     },
   },
