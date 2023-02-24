64fe2068e7bec68c7a7e0c113bd5e263be227386
Esteban Sayago <sayagoo823@gmail.com>
2023-02-24 10:19:44 -0300

    eliminar etiqueta vacia

--- src/components/FondoDePagina.jsx
+++ src/components/FondoDePagina.jsx
@@ -6,12 +6,14 @@ import { ThemeContext } from "../context/darkTheme"
 function FondoDePagina() {
     const [color] = useContext(ThemeContext)
     return (
-        <>
-            <div
-                className="absolute top-0 left-0 -z-10 h-screen w-screen bg-cover bg-no-repeat"
-                style={{ backgroundImage: `url(${color === "dark" ? bgOscuro : bgClaro})` }}
-            ></div>
-        </>
+        <div
+            className="absolute top-0 left-0 -z-10 h-screen w-screen bg-cover bg-no-repeat"
+            style={{
+                backgroundImage: `url(${
+                    color === "dark" ? bgOscuro : bgClaro
+                })`,
+            }}
+        ></div>
     )
 }
 
