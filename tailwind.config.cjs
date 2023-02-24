415aca6e1b50c721ea4afda5eb5b4a479566b1b5
Esteban Sayago <sayagoo823@gmail.com>
2023-02-24 10:19:06 -0300

    agregar animacion de paginas

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,20 +4,25 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        "text-focus-in": "text-focus-in 1.7s cubic-bezier(0.550, 0.085, 0.680, 0.530) 0.5s both",
-        "backanimado": "backanimado 30s ease infinite"
+        "text-focus-in": "text-focus-in 1.9s cubic-bezier(0.550, 0.085, 0.680, 0.530) 0.5s both",
+        "backanimado": "backanimado 30s ease infinite",
+        "entrada-suave": "entrada-suave .6s cubic-bezier(0.680, -0.550, 0.365, 1.390)"
       }
       ,
       keyframes: {
         "text-focus-in": {
-          "0%": { filter: "blur(9px)", opacity: "0" },
-          "100%": { filter: "blur(0px)", opacity: "1" }
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" }
         },
         "backanimado": {
           "0%": { "background-position": "99% 0%" },
           "50%": { "background-position": "2% 100%" },
           "100%": { "background-position": "99% 0%" }
         },
+        "entrada-suave": {
+          "0%": { transform: "translateY(200%)" },
+          "100%": { transform: "translateY(0)" }
+        }
       }
     },
   },
