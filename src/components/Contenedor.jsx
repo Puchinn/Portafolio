da1e9ec3489aaff499f0d1e105ee0475546cef6d
Esteban Sayago <sayagoo823@gmail.com>
2023-02-24 13:50:15 -0300

    aincluir componente Menu y ajustar opacidad del fondo

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -1,15 +1,13 @@
-import Menu from "./Menu/Menu"
 import { Outlet } from "react-router-dom"
-import MenuResponsive from "./menuResponsive/MenuResponsive"
+import Menu from "./menu/Menu"
 
 function Contenedor() {
     return (
         <div className="flex h-screen w-screen items-center justify-center px-1 py-1">
-            <div className="relative  h-full w-full overflow-y-scroll rounded-md bg-white bg-opacity-50 dark:border-none dark:bg-black dark:bg-opacity-50 md:rounded-2xl xl:grid xl:max-h-[700px] xl:w-5/6 xl:max-w-7xl xl:items-center xl:overflow-hidden">
-                <MenuResponsive />
+            <div className="relative h-full w-full overflow-y-scroll rounded-md border bg-white bg-opacity-50  dark:bg-black dark:bg-opacity-60 md:rounded-2xl xl:max-h-[700px] xl:w-5/6 xl:max-w-7xl">
+                <Menu />
                 <Outlet />
             </div>
-            <Menu />
         </div>
     )
 }
