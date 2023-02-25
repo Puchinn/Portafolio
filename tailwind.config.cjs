c09f8363a3e3907df25c18f9a8afb0d14b78703e
Esteban Sayago <sayagoo823@gmail.com>
2023-02-25 20:33:11 -0300

    eliminar animaciones de la pagina intro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,21 +4,10 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        "text-focus-in": "text-focus-in 1.9s cubic-bezier(0.550, 0.085, 0.680, 0.530) 0.5s both",
-        "backanimado": "backanimado 30s ease infinite",
         "entrada-suave": "entrada-suave .6s cubic-bezier(0.880, -0.550, 0.365, 1.390)"
       }
       ,
       keyframes: {
-        "text-focus-in": {
-          "0%": { opacity: "0" },
-          "100%": { opacity: "1" }
-        },
-        "backanimado": {
-          "0%": { "background-position": "99% 0%" },
-          "50%": { "background-position": "2% 100%" },
-          "100%": { "background-position": "99% 0%" }
-        },
         "entrada-suave": {
           "0%": { transform: "translateY(300%)" },
           "100%": { transform: "translateY(0)" }
