cc969cc5a4c084ff9bf3915518dd80b331742618
Esteban Sayago <sayagoo823@gmail.com>
2023-02-25 23:39:58 -0300

    estilar version responsive

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,19 +2,21 @@ import svg from "../assets/programando.svg"
 
 function Inicio() {
     return (
-        <div className="flex animate-entrada-suave justify-center space-x-12 pt-28">
-            <div className="w-96">
-                <img src={svg} alt="" />
+        <div className="flex animate-entrada-suave flex-col justify-center pt-8 pb-4 sm:pt-14 lg:flex-row lg:items-center lg:space-x-7 lg:pt-28">
+            <div className="mx-auto w-60 pb-3 sm:w-72 lg:mx-0 lg:w-96">
+                <img src={svg} alt="svg programador" />
             </div>
-            <div className="flex flex-col justify-center text-center">
-                <h1 className="text-6xl font-bold text-[#fa5f49]">
+            <div className="px-3 text-center">
+                <h1 className="text-3xl font-bold text-[#fa5f49] sm:text-5xl">
                     ESTEBAN SAYAGO
                 </h1>
-                <h1 className="py-3 text-4xl font-light text-gray-800 dark:text-white">
-                    Desarrollador web Front-End
+                <h1 className="pb-4 text-2xl font-light text-gray-800 dark:text-gray-100 sm:text-4xl">
+                    Front-End Dev
                 </h1>
-                <p className="max-w-lg text-gray-700 dark:text-white">
-                ¡ Saludos ! Soy Desarrollador Trainer/Junior, con 1 año de experiencia en el desarrollo web. Te invito a continuar revisando mi portafolio y a que conozcas mas sobre mi.
+                <p className="mx-auto max-w-lg text-lg text-gray-700 dark:text-white">
+                    ¡ Saludos ! Soy Desarrollador Trainer/Junior, con 1 año de
+                    experiencia en el desarrollo web. Te invito a continuar
+                    revisando mi portafolio y a que conozcas mas sobre mi.
                 </p>
             </div>
         </div>
