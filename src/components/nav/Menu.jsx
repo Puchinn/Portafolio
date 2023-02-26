59fd1be2a527a805070d04f6b0a5fdbb20a8ac57
Esteban Sayago <sayagoo823@gmail.com>
2023-02-25 21:01:18 -0300

    agregar emojis al texto de los links

--- src/components/nav/Menu.jsx
+++ src/components/nav/Menu.jsx
@@ -4,9 +4,10 @@ import ToggleButton from "./ToggleButton"
 function Menu() {
     return (
         <nav className="sticky top-0 z-10 flex h-min justify-around space-x-1 rounded-b-2xl bg-white bg-opacity-60 p-2 text-gray-400 backdrop-blur-sm dark:bg-black dark:bg-opacity-40 md:justify-start md:space-x-5 md:px-4">
-            <MenuLink to={"sobremi"} text="Home" />
-            <MenuLink to={"portafolio"} text="Portafolio" />
-            <MenuLink to={"contacto"} text="Contacto" />
+            <MenuLink to={"/"} text="🏠 Inicio" />
+            <MenuLink to={"sobremi"} text="📋 Sobre mi" />
+            <MenuLink to={"portafolio"} text="💼 Portafolio" />
+            <MenuLink to={"contacto"} text="🚀 Contacto" />
             <ToggleButton />
         </nav>
     )
