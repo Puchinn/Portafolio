7f1aa0d28ccff24902c5900bcee08c92a76f5dbc
Esteban Sayago <sayagoo823@gmail.com>
2023-02-26 01:24:44 -0300

    restaurar estilos

--- src/components/FondoDePagina.jsx
+++ src/components/FondoDePagina.jsx
@@ -1,18 +1,18 @@
-// import fondoClaro from "../assets/fondos/fondoClaro.png"
-// import fondoOscuro from "../assets/fondos/fondoOscuro.png"
+import fondoClaro from "../assets/fondos/fondoClaro.png"
+import fondoOscuro from "../assets/fondos/fondoOscuro.png"
 import { useContext } from "react"
 import { ThemeContext } from "../context/darkTheme"
 
 function FondoDePagina() {
-    // const [color] = useContext(ThemeContext)
+    const [color] = useContext(ThemeContext)
     return (
         <div
             className="absolute top-0 left-0 -z-10 h-screen w-screen  bg-cover  bg-no-repeat"
-            // style={{
-            //     backgroundImage: `url(${
-            //         color === "dark" ? fondoOscuro : fondoClaro
-            //     })`,
-            // }}
+            style={{
+                backgroundImage: `url(${
+                    color === "dark" ? fondoOscuro : fondoClaro
+                })`,
+            }}
         ></div>
     )
 }
