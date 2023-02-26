6bba03fa5263ae8bfa5007be9ce906774bb770d4
Esteban Sayago <sayagoo823@gmail.com>
2023-02-25 23:06:48 -0300

    estilar version movil

--- src/components/nav/Menu.jsx
+++ src/components/nav/Menu.jsx
@@ -1,15 +1,25 @@
 import MenuLink from "./MenuLink"
 import ToggleButton from "./ToggleButton"
+import { useState } from "react"
+import BotonMenu from "./BotonMenu"
 
 function Menu() {
+    const [isActive, setIsActive] = useState(false)
     return (
-        <nav className="sticky top-0 z-10 flex h-min justify-around space-x-1 rounded-b-2xl bg-white bg-opacity-60 p-2 text-gray-400 backdrop-blur-sm dark:bg-black dark:bg-opacity-40 md:justify-start md:space-x-5 md:px-4">
-            <MenuLink to={"/"} text="🏠 Inicio" />
-            <MenuLink to={"sobremi"} text="📋 Sobre mi" />
-            <MenuLink to={"portafolio"} text="💼 Portafolio" />
-            <MenuLink to={"contacto"} text="🚀 Contacto" />
-            <ToggleButton />
-        </nav>
+        <div className="sticky top-0 z-10 h-min rounded-b-2xl bg-white bg-opacity-70 p-2 text-gray-400 backdrop-blur-sm dark:bg-black dark:bg-opacity-40">
+            <BotonMenu isActive={isActive} setIsActive={setIsActive} />
+            <nav
+                className={`flex flex-col ${
+                    isActive ? "block" : "hidden"
+                } justify-around space-x-1 md:flex md:flex-row md:justify-start md:space-x-5 md:px-4`}
+            >
+                <MenuLink to={"/"} text="🏠 Inicio" />
+                <MenuLink to={"sobremi"} text="📋 Sobre mi" />
+                <MenuLink to={"portafolio"} text="💼 Portafolio" />
+                <MenuLink to={"contacto"} text="🚀 Contacto" />
+                <ToggleButton />
+            </nav>
+        </div>
     )
 }
 
