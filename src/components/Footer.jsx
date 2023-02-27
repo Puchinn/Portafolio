057d7215d79e6e21bcb6b6eacdb1b523b525aef9
Esteban Sayago <sayagoo823@gmail.com>
2023-02-27 15:20:15 -0300

    crear componente footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer (){
+    return (
+        <h1 className="text-xs text-center py-5 text-gray-800 dark:text-white animate-entrada-suave ">Portafolio hecho por Esteban Sayago en react ©2023</h1>
+    )
+}
+
+export default Footer
