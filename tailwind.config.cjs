7ca9fce8f8f3e7f6f1040dc4c52decacee5c4ff5
Esteban Sayago <sayagoo823@gmail.com>
2023-02-27 16:35:16 -0300

    eliminar animacion de paginas

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,18 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {
-      animation: {
-        "entrada-suave": "entrada-suave .6s cubic-bezier(0.880, -0.550, 0.365, 1.390)"
-      }
-      ,
-      keyframes: {
-        "entrada-suave": {
-          "0%": { transform: "translateY(300%)" },
-          "100%": { transform: "translateY(0)" }
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
   darkMode: 'class'
