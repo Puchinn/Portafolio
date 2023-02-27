4e8d92275b4a3391b7e6df7ce9016c62e0d2552c
Esteban Sayago <sayagoo823@gmail.com>
2023-02-27 15:19:55 -0300

    agregar padding inferior

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom"
 function Contenedor() {
     return (
         <div className="flex h-screen w-screen items-center justify-center px-1 py-1">
-            <div className="relative h-full w-full overflow-y-scroll rounded-md border bg-white bg-opacity-50  dark:bg-black dark:bg-opacity-60 md:rounded-2xl xl:max-h-[700px] xl:w-5/6 xl:max-w-7xl">
+            <div className="relative h-full w-full pb-5 overflow-y-scroll rounded-md border bg-white bg-opacity-50  dark:bg-black dark:bg-opacity-60 md:rounded-2xl xl:max-h-[700px] xl:w-5/6 xl:max-w-7xl">
                 <Menu />
                 <Outlet />
             </div>
