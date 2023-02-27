667cb800699c5d3744c5ec3f20f764f7eb170e63
Esteban Sayago <sayagoo823@gmail.com>
2023-02-27 18:26:42 -0300

    crear el router en main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import Router from "./router/router"
 import "./main.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import App from "./App"
+import SobreMi from "./pages/Sobre_Mi/SobreMi"
+import Contacto from "./pages/Contacto/Contacto"
+import Portafolio from "./pages/Portafolio"
+import Inicio from "./pages/Inicio"
+
+const rutas = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Inicio />,
+            },
+            {
+                path: "/sobremi",
+                element: <SobreMi />,
+            },
+            {
+                path: "/portafolio",
+                element: <Portafolio />,
+            },
+            {
+                path: "/contacto",
+                element: <Contacto />,
+            },
+        ],
+    },
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Router />
+        <RouterProvider router={rutas} />
     </React.StrictMode>
 )
