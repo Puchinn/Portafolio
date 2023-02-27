14ac5c33e2e2a894b0aeab40f80044bbae7424f2
Esteban Sayago <sayagoo823@gmail.com>
2023-02-27 18:07:52 -0300

    bajar el contexto a nivel de App

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import Router from "./router/router"
 import "./main.css"
-import ContextContainer from "./context/darkTheme"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <ContextContainer>
-            <Router />
-        </ContextContainer>
+        <Router />
     </React.StrictMode>
 )
