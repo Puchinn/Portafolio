88e68a464b00c49e415c57cbadf206dae7c8b7c9
Esteban Sayago <sayagoo823@gmail.com>
2023-02-27 11:00:07 -0300

    componer el componente 'contenedor' directamente en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import FondoDePagina from "./components/FondoDePagina"
-import Contenedor from "./components/Contenedor"
+import Menu from "./components/nav/Menu"
+import { Outlet } from "react-router-dom"
 
 function App() {
     return (
-        <>
-            <Contenedor />
-            <FondoDePagina />
-        </>
+        <div className="flex h-screen w-screen items-center justify-center px-1 py-1">
+            <div className="relative h-full w-full overflow-y-scroll rounded-md border bg-white bg-opacity-50  dark:bg-black dark:bg-opacity-60 md:rounded-2xl xl:max-h-[700px] xl:w-5/6 xl:max-w-7xl">
+                <Menu />
+                <Outlet />
+            </div>
+        </div>
     )
 }
 
