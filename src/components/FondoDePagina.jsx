51fd5f8472b984d9d36b4e33ae3af7632ba0e2b3
Esteban Sayago <sayagoo823@gmail.com>
2023-02-27 09:49:13 -0300

    agregar a cache las imagenes de fondo

--- src/components/FondoDePagina.jsx
+++ src/components/FondoDePagina.jsx
@@ -1,10 +1,27 @@
 import fondoClaro from "../assets/fondos/fondoClaro.png"
 import fondoOscuro from "../assets/fondos/fondoOscuro.png"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { ThemeContext } from "../context/darkTheme"
+
+function cacheImages(images) {
+    return images.map((src) => {
+        return new Promise((resolve, reject) => {
+            const img = new Image()
+            img.src = src
+            img.onload = resolve
+            img.onerror = reject
+        })
+    })
+}
 
 function FondoDePagina() {
     const [color] = useContext(ThemeContext)
+
+    useEffect(() => {
+        const images = [fondoOscuro, fondoClaro]
+        cacheImages(images)
+    }, [color])
+
     return (
         <div
             className="absolute top-0 left-0 -z-10 h-screen w-screen  bg-cover  bg-no-repeat"
