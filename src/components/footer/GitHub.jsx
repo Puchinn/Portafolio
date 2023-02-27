e981801184f3a259abac67012435b40ea61c26d5
Esteban Sayago <sayagoo823@gmail.com>
2023-02-27 18:07:14 -0300

    fix:atributos de svgs en react

--- src/components/footer/GitHub.jsx
+++ src/components/footer/GitHub.jsx
@@ -7,13 +7,13 @@ function GitHub() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
                 stroke="#000000"
-                stroke-width="0.72"
+                strokeWidth="0.72"
             >
-                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                 <g
                     id="SVGRepo_tracerCarrier"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 ></g>
                 <g id="SVGRepo_iconCarrier">
                     {" "}
