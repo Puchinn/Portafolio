d0f8bc8200925ece73c0372f3d010d8c93871daa
Esteban Sayago <sayagoo823@gmail.com>
2023-02-27 21:40:22 -0300

    mover el boton de cambiar tema al nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import FondoDePagina from "./components/FondoDePagina"
 import Contenedor from "./components/Contenedor"
 import ContextContainer from "./context/darkTheme"
-import DarkModeButton from "./components/DarkModeButton"
 
 function App() {
     return (
         <ContextContainer>
             <Contenedor />
             <FondoDePagina />
-            <DarkModeButton />
         </ContextContainer>
     )
 }
