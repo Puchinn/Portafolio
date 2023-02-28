5f2c2a034ee746e18a959f3fa40670b65ec20fbc
Esteban Sayago <sayagoo823@gmail.com>
2023-02-27 21:42:20 -0300

    integrar boton cambiar de tema

--- src/components/nav/Menu.jsx
+++ src/components/nav/Menu.jsx
@@ -1,15 +1,15 @@
 import MenuLink from "./MenuLink"
-import ToggleButton from "./ToggleButton"
 import { useState } from "react"
 import BotonMenu from "./BotonMenu"
+import DarkModeButton from "../DarkModeButton"
 
 function Menu() {
     const [isActive, setIsActive] = useState(false)
     return (
-        <div className="sticky top-0 z-10 h-min rounded-b-2xl bg-white bg-opacity-70 p-2 text-gray-400 backdrop-blur-sm dark:bg-black dark:bg-opacity-40 md:flex md:flex-row-reverse md:justify-end">
+        <div className="sticky top-0 z-10 h-min rounded-b-2xl bg-white bg-opacity-70 p-2 text-gray-400 backdrop-blur-sm dark:bg-black dark:bg-opacity-40 md:flex md:flex-row-reverse md:justify-between">
             <div className="flex justify-between">
                 <BotonMenu isActive={isActive} setIsActive={setIsActive} />
-                <ToggleButton />
+                <DarkModeButton />
             </div>
             <nav
                 className={`flex flex-col ${
@@ -21,6 +21,7 @@ function Menu() {
                 <MenuLink to={"portafolio"} text="💼 Portafolio" />
                 <MenuLink to={"contacto"} text="🚀 Contacto" />
             </nav>
+            
         </div>
     )
 }
