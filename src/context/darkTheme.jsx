c7870fc5d3d88b8818977bc3efc82f92681977b8
Esteban Sayago <sayagoo823@gmail.com>
2023-06-07 19:12:12 -0300

    refactorizar

--- src/context/darkTheme.jsx
+++ src/context/darkTheme.jsx
@@ -1,32 +1,21 @@
-import { useState, createContext, useEffect } from "react"
+import { useState, createContext } from 'react'
 
 export const ThemeContext = createContext(null)
 
 function ContextContainer({ children }) {
-    const [color, setColor] = useState(null)
+  const [color, setColor] = useState('dark')
 
-    useEffect(() => {
-        const localColorMode = localStorage.getItem("colorMode")
-        if (localColorMode) {
-            setColor(localColorMode)
-        } else {
-            const colorModeQuery = window.matchMedia(
-                "(prefers-color-scheme: dark)"
-            )
-            setColor(colorModeQuery.matches ? "dark" : "light")
-            colorModeQuery.addEventListener("change", (event) => {
-                setColor(event.matches ? "dark" : "light")
-            })
-        }
-    }, [])
-
-    return (
-        <ThemeContext.Provider value={[color, setColor]}>
-            <div className={`App ${color === "dark" ? "dark" : ""} flex h-screen w-screen items-center justify-center px-1 py-1`}>
-                {children}
-            </div>
-        </ThemeContext.Provider>
-    )
+  return (
+    <ThemeContext.Provider value={[color, setColor]}>
+      <div
+        className={`App ${
+          color ?? ''
+        } flex h-screen w-screen items-center justify-center px-1 py-1`}
+      >
+        {children}
+      </div>
+    </ThemeContext.Provider>
+  )
 }
 
 export default ContextContainer
