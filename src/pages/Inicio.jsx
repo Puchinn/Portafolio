9c3e163ddfa03b29f26137ac1e20c80de97df462
Esteban Sayago <sayagoo823@gmail.com>
2023-06-22 18:01:41 -0300

    hacer prueba de paginas

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,32 +1,32 @@
-import svg from "../assets/programan2.svg"
+import perfil from '../assets/perfil2.png'
 
 function Inicio() {
-    return (
-        <div className="animate-entrada-suave flex flex-col justify-center pt-8 pb-4 sm:pt-14 lg:flex-row lg:items-center lg:space-x-7 lg:pt-28">
-            <div className="mx-auto pb-3 lg:mx-0">
-                <img
-                    className="w-72 lg:w-96"
-                    src={svg}
-                    alt="svg programador"
-                    width={270}
-                    height={195}
-                />
-            </div>
-            <div className="px-3 text-center">
-                <h1 className="text-3xl font-bold text-[#fa5f49] sm:text-5xl">
-                    ESTEBAN SAYAGO
-                </h1>
-                <h1 className="pb-4 text-2xl font-light text-gray-800 dark:text-gray-100 sm:text-4xl">
-                    Front-End Dev
-                </h1>
-                <p className="mx-auto max-w-lg text-lg text-gray-700 dark:text-white">
-                    Saludos desde Córdoba, Argentina. Este es mi portafolio
-                    personal donde te invito a que conozcas acerca de mí, mis
-                    objetivos y mis habilidades en el mundo del desarrollo web.
-                </p>
-            </div>
-        </div>
-    )
+  return (
+    <div className='flex animate-entrada-suave flex-col justify-center pt-8 pb-4 sm:pt-14 lg:flex-row lg:items-center lg:space-x-7'>
+      <div className='mx-auto pb-3 lg:mx-0'>
+        <img
+          className='w-72 rounded-2xl lg:w-96'
+          src={perfil}
+          alt='svg programador'
+          width={270}
+          height={195}
+        />
+      </div>
+      <div className='px-3 text-center'>
+        <h1 className='text-3xl font-bold text-[#fa5f49] sm:text-5xl'>
+          ESTEBAN SAYAGO
+        </h1>
+        <h1 className='pb-4 text-2xl font-light text-gray-800 dark:text-gray-100 sm:text-4xl'>
+          Front-End Dev
+        </h1>
+        <p className='mx-auto max-w-lg text-lg text-gray-700 dark:text-white'>
+          Saludos desde Córdoba, Argentina. Este es mi portafolio personal donde
+          te invito a que conozcas acerca de mí, mis objetivos y mis habilidades
+          en el mundo del desarrollo web.
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Inicio
--- src/pages/Portafolio.jsx
+++ src/pages/Portafolio.jsx
@@ -1,37 +1,82 @@
-import Titulo from "../components/Titulo"
+import Titulo from '../components/Titulo'
+import test from '../assets/test2.jpg'
 
 function Portafolio() {
-    return (
-        <div className="animate-entrada-suave text-slate-700 dark:text-white">
-            <Titulo titulo={"Portafolio"} />
-            <div className="text-center">
-                <h1 className="py-3 text-4xl sm:text-6xl ">
-                    Y los proyectos ? 😳
-                </h1>
-                <h1 className="text-3xl sm:text-5xl">(work in progress)</h1>
+  return (
+    <div className='animate-entrada-suave text-slate-700 dark:text-white'>
+      <Titulo titulo={'Proyectos'} />
+      <div class='mx-auto max-w-5xl px-4 py-8'>
+        <section class='rounded-lg bg-gray-100/10 p-8'>
+          <div class='grid grid-cols-1 lg:grid-cols-3 lg:items-center lg:gap-12'>
+            <img
+              alt='Man'
+              src={test}
+              class='aspect-square max-h-[300px] w-full rounded-lg object-cover object-left-top'
+            />
+
+            <blockquote class='sm:col-span-2'>
+              <h2 className='text-lg font-bold'>Pagina de Peliculas</h2>
+              <p class='text-xl font-medium sm:text-xl'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                perspiciatis cumque neque ut nobis excepturi, quasi iure
+                quisquam autem alias.
+              </p>
+
+              <cite class='mt-8 inline-flex items-center not-italic'>
+                <span class='hidden h-px w-6 bg-gray-400 sm:inline-block'></span>
+                <p class='sm:ms-3 text-sm uppercase text-gray-500'>
+                  <strong>Justin Harrison</strong>, Beatle Inc.
+                </p>
+              </cite>
+              <div className='flex justify-end space-x-3 py-3'>
                 <a
-                    className="mt-6 inline-flex items-center gap-2 rounded dark:bg-white dark:border-white dark:text-slate-700 dark:hover:bg-transparent dark:hover:text-white border-2 border-slate-700 bg-slate-700 px-5 py-3 text-sm font-medium text-white transition-colors hover:bg-transparent hover:text-slate-700 focus:outline-none focus:ring active:opacity-75"
-                    href="https://github.com/Puchinn"
-                    target="_blank"
-                    rel="noreferrer"
+                  class='inline-flex items-center gap-2 rounded border-2 border-[#ea4c89] bg-[#ea4c89] px-5 py-3 text-sm font-medium text-white transition-colors hover:bg-transparent hover:text-[#ea4c89] focus:outline-none focus:ring active:opacity-75'
+                  href='/dribbble'
+                  target='_blank'
+                  rel='noreferrer'
                 >
-                    Ver GitHub
-                    <svg
-                        className="h-5 w-5"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                        aria-hidden="true"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                            clipRule="evenodd"
-                        ></path>
-                    </svg>
+                  Ver Pagina
+                  <svg
+                    class='h-5 w-5'
+                    fill='currentColor'
+                    viewBox='0 0 24 24'
+                    aria-hidden='true'
+                  >
+                    <path
+                      fill-rule='evenodd'
+                      d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z'
+                      clip-rule='evenodd'
+                    ></path>
+                  </svg>
                 </a>
-            </div>
-        </div>
-    )
+
+                <a
+                  class='inline-flex items-center gap-2 rounded border-2 border-[#171515] bg-[#171515] px-5 py-3 text-sm font-medium text-white transition-colors hover:bg-transparent hover:text-[#171515] focus:outline-none focus:ring active:opacity-75'
+                  href='/github'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Repositorio
+                  <svg
+                    class='h-5 w-5'
+                    fill='currentColor'
+                    viewBox='0 0 24 24'
+                    aria-hidden='true'
+                  >
+                    <path
+                      fill-rule='evenodd'
+                      d='M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z'
+                      clip-rule='evenodd'
+                    ></path>
+                  </svg>
+                </a>
+              </div>
+            </blockquote>
+          </div>
+        </section>
+      </div>
+    </div>
+  )
 }
 
 export default Portafolio
