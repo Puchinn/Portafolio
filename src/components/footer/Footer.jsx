69b656897bf103efcc14f6de73c6b97e1e1c0697
Esteban Sayago <sayagoo823@gmail.com>
2023-06-21 22:24:47 -0300

    refactor: dividir archivos en carpetas

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,20 @@
-import Gmail from "./Gmail"
-import GitHub from "./GitHub"
-import Linkedin from "./Linkedin"
+import Gmail from './components/Gmail'
+import GitHub from './components/GitHub'
+import Linkedin from './components/Linkedin'
 
 function Footer() {
-    return (
-        <div className="pt-8">
-            <div className="flex items-center justify-center space-x-2">
-                <Gmail />
-                <GitHub />
-                <Linkedin />
-            </div>
-            <h1 className="py-3 text-center text-sm text-gray-800 dark:text-white ">
-                Portafolio hecho por Esteban Sayago en react ©2023
-            </h1>
-        </div>
-    )
+  return (
+    <footer className='pt-8'>
+      <div className='flex items-center justify-center space-x-2'>
+        <Gmail />
+        <GitHub />
+        <Linkedin />
+      </div>
+      <h1 className='py-3 text-center text-sm text-gray-800 dark:text-white '>
+        Portafolio hecho por Esteban Sayago en react ©2023
+      </h1>
+    </footer>
+  )
 }
 
 export default Footer
