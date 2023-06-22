2645947b232953f21b25f6af8103ad3766d919c1
Esteban Sayago <sayagoo823@gmail.com>
2023-06-21 22:24:10 -0300

    agregar animacion para paginas

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "entrada-suave": "entrada-suave 0.3s cubic-bezier(0.175, 0.885, 0.320, 1.275) both",
+      },
+      keyframes: {
+        "entrada-suave": {
+          "0%": {
+            transform: "translateY(100px)"
+          },
+          "100%": {
+            transform: "translateY(0)"
+          }
+        }
+      }
+    },
   },
   plugins: [],
   darkMode: 'class'
